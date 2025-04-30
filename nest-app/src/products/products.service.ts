02df91e98165ae8b83287d9bc815e7daae409468
MDRWP-GH <117169377+MDRWP-GH@users.noreply.github.com>
2025-04-30 16:33:08 +0700

    Thirt commit

--- nest-app/src/products/products.service.ts
+++ nest-app/src/products/products.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-    private readonly products = [
-        {
-            id: 1,
-            name: 'Product 1',
-            description: 'Description for Product 1',
-        },
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
+  }
 
-        {
-            id: 2,
-            name: 'Product 2',
-            description: 'Description for Product 2',
-        },
-    ];
+  findAll() {
+    return `This action returns all products`;
+  }
 
-    findAll() {
-        return this.products;
-    }
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
 
-    create(product) {
-        this.products.push(product);
-        return product;
-    }
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
 }
