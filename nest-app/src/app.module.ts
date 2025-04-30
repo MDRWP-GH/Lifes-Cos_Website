0ca444019aba063321216ea2b3b8de51c2bccda0
MDRWP-GH <117169377+MDRWP-GH@users.noreply.github.com>
2025-04-30 12:01:14 +0700

    First Request

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AttractionsModule } from './attractions/attractions.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AttractionsModule],
+  imports: [ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
---
+++ nest-app/src/products/products.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductsService {
+    private readonly products = [
+        {
+            id: 1,
+            name: 'Product 1',
+            description: 'Description for Product 1',
+        },
+
+        {
+            id: 2,
+            name: 'Product 2',
+            description: 'Description for Product 2',
+        },
+    ];
+
+    findAll() {
+        return this.products;
+    }
+
+    create(product) {
+        this.products.push(product);
+        return product;
+    }
+}
