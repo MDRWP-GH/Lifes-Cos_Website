bf7db67da61a85195846f812fc7857a3f47d2406
MDRWP-GH <117169377+MDRWP-GH@users.noreply.github.com>
2025-05-02 00:16:37 +0700

    Second Front-end

--- next-app/components/Navbar.js
+++ next-app/components/Navbar.js
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   const [visible, setVisible] = useState(true);
@@ -22,23 +24,26 @@ export default function Navbar() {
     };
 
     window.addEventListener("scroll", handleScroll);
-
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
 
   return (
     <nav
-      className={`fixed top-0 left-0 w-full shadow-md transition-transform duration-300 z-50 ${
+      className={`fixed top-0 left-0 w-full bg-black shadow-md transition-transform duration-300 z-50 ${
         visible ? "translate-y-0" : "-translate-y-full"
       }`}
     >
-      <div className="max-w-6xl mx-auto p-4 flex justify-between items-center">
-        <span className="font-bold text-xl">MySite</span>
-        <ul className="flex space-x-4">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+      
+      <div className="max-w-6xl mx-auto p-6 flex justify-between items-center">
+        <Link href="/" className="flex items-center space-x-2">
+          <Image href="#home" src="/Lifes’s Cos White.png" alt="Life'Cos Logo" width={175} height={175}/>
+        </Link>
+
+        <div className="hidden md:flex space-x-4">
+          <Link href="#home" className="text-white">Home</Link>
+          <Link href="#about" className="text-white">About</Link>
+          <Link href="#contact" className="text-white">Contact</Link>
+        </div>
       </div>
     </nav>
   );
